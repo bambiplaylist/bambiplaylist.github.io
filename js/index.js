a6d1ec5c668720de40c71d548b3f5cab2c091926
katiegirlsarah <69067202+katiegirlsarah@users.noreply.github.com>
2021-06-20 12:29:12 +1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -118,11 +118,13 @@ const compare = (e) =>{
 function allowDrop(ev) {
     ev.preventDefault();
     draggedOver = ev.target.innerHTML.split("href=\"")[1].split("\">")[0];
+    console.log(draggedOver+" do");
     audios = [];
 }
 
 const setDragging = (e) =>{
   dragging = e.target.innerHTML.split("href=\"")[1].split("\">")[0];
+    console.log(dragging+" d");
   audios = [];
 }
 
