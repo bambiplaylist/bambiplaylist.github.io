f9a6c6865e9e0b9fd81996307b739f0a12c5d3b1
katiegirlsarah <katiegirlsarah@gmail.com>
2022-05-10 07:33:48 +0000

    fix caching

--- sw.js
+++ sw.js
@@ -15,21 +15,11 @@ self.addEventListener("install", (event) => {
 	);
 });
 
-const putInCache = async (request, response) => {
-	const cache = await caches.open("v1");
-	await cache.put(request, response);
-}
-
-const cacheFirst = async (request) => {
+self.addEventListener('fetch', (event) => {
 	const responseFromCache = await caches.match(request);
 	if (responseFromCache) {
 		return responseFromCache;
+	} else {
+		return fetch(request)
 	}
-	const responseFromNetwork = await fetch(request);
-	putInCache(request, responseFromNetwork.clone())
-	return responseFromNetwork;
-};
-
-self.addEventListener('fetch', (event) => {
-	event.respondWith(cacheFirst(event.request));
 });
